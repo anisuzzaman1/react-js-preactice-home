3fe69a51d160810b34aced0185d77fa9fea39bc5
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-02-19 01:06:08 +0600

    React Apps Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Friend name="Nahid" age="41"></Friend>
+      <Friend name="Easin" age="40"></Friend>
+    </div>
+  );
+}
+
+/* <Person name="Rubel" job="Acting"></Person>
+      <Person name="Mousumi" job="Actress"></Person>
+      <Person></Person> */
+
+// function Person(props) {
+//   console.log(props);
+//   const Person = {
+//     backgroundColor: 'lightgoldenrodyellow',
+//     color: 'crimson',
+//     border: '3px solid',
+//     borderRadius: '15px',
+//     padding: '15px'
+//   };
+
+// return (
+//   <div style={Person}>
+//     <h1>Name: {props.name}</h1>
+//     <h4>Profession: {props.job}</h4>
+//   </div >
+// );
+
+function Friend(props) {
+  return (
+    <div className='Person'>
+      <h3>
+        Name:{props.name}
+      </h3>
+      <h5>
+        Age:{props.age}
+      </h5>
     </div>
   );
 }
