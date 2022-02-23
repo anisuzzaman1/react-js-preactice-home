3625f404dc8cf41965b03942c7ded5a79fb6b3af
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-02-24 02:02:36 +0600

    Add Final Country Compo

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -4,8 +4,8 @@ import './Country.css'
 const Country = (props) => {
     const { name, flag, capital, timezones, nativeName } = props.country;
     return (
-        <div className='country'>
-            <h3>{name}</h3>
+        <div className='country bg-success bg-gradient'>
+            <h3 className='text-wrap fs-1 text'>{name}</h3>
             <p><img src={flag}></img></p>
             <p>Capital: {capital}</p>
             <p>Time Zone: {timezones[0]}</p>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p><small>2022 | by Anisuzzaman </small></p>
+        </div>
+    );
+};
+
+export default Footer;
