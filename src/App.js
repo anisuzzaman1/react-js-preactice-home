a21651e4c9fd364e7f5cdab13e2fd8462adbc15b
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-02-23 15:58:26 +0600

    Load External Data into React

--- src/App.js
+++ src/App.js
@@ -1,69 +1,60 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
-    </div >
+      {/* <Counter></Counter> */}
+      <ExternalUser></ExternalUser>
+    </div>
   );
 }
 
+function ExternalUser() {
+  const [users, setUsers] = useState([]); // Arrary Default value []
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+  return (
+    <div>
+      <h1>External Users</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  );
+}
+
+function User(props) {
+  return (
+    <div className='product'>
+      <h2>{props.name}</h2>
+      <p>Email: {props.email}</p>
+    </div>
+  );
+}
+
+
 function Counter() {
   const [count, setCount] = useState(0);
-  const handleIncrease = () => {
+  const handleIncrese = () => {
     const newCount = count + 1;
     setCount(newCount);
   }
-
   const handleDecrease = () => {
     const newCount = count - 1;
     setCount(newCount);
   }
-
-  return (
-    <div className='product'>
-      <h1>Count: {count}</h1>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button>
-      <ExternalUsers></ExternalUsers>
-    </div>
-  )
-}
-
-function ExternalUsers() {
   return (
     <div>
-      <h3>External Users: </h3>
-    </div>
-  )
-}
-
-function Product(props) {
-  return (
-    <div className='product'>
-      <h2>{props.name} </h2>
-      <h3>Price: {props.price}</h3>
+      <h1>Counter: {count}</h1>
+      <button onClick={handleIncrese}>Increase (+)</button>
+      <button onClick={handleDecrease}>Decrease (-)</button>
     </div>
   );
 }
+
 export default App;
-
-/*
- const products = [
-    { name: 'iPhone13', price: "100000" },
-    { name: 'Macbook Air', price: "120000" },
-    { name: 'Apple Watch', price: "40000" }
-  ]
-  const price = ['100000', '120000', '40000'];
-*/
-
-/* 
-{
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-      {/* <Product name={product[0]} price={price[0]}> </Product>
-      <Product name={product[1]} price={price[1]}> </Product>
-      <Product name={product[2]} price={price[2]}> </Product> }
-*/
